bb67436088b7c48e7cb2a5228d266e9ca3ab2209
Jason Vanderheyden <jason.vanderheyden@yahoo.com>
2020-01-04 10:48:14 -0500

    content update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Simple Blog`,
+    description: `This is a simple blog I developed to test using WordPress as a CMS with Gatsby.js`,
+    author: `@theremotecreative`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,8 +24,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-simple-blog`,
+        short_name: `blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -73,6 +73,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,37 +6,20 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Headless CMS Gatsby Base Template</h1>
-    <p>Welcome to your new Gatsby site. This site is a base template for building something amazing with Gatsby while using WordPress as a headless CMS </p>
-    <p>This is Version 2.0. Delete this content to create a new home page</p>
+    <p>Welcome to my simple Gatsby blog. This site is a base template for building something amazing with Gatsby while using WordPress as a CMS </p>
+    <p>This is Version 2.0.</p>
     <p>This version contains the following features out of the box:</p>
     <ul>
-      <li>Pulls and renders posts and pages from a wordpress site</li>
-      <li>Pulls and renders a custom post type called "Project" which uses ACF fields</li>
+      <li>Creates posts and pages from a WordPress API</li>
+      <li>Creates a custom post type called "Project" which uses ACF fields</li>
       <li>Portfolio page displays custom post type loop</li>
-      <li>Menu has links from API but hardcoded page links</li>
       <li>Categories are included in post and custom post type single posts</li>
       <li>Category archive template for posts and custom posts</li>
       <li>Includes a simple menu and mobile menu</li>
       <li>Includes a simple fade page transition</li>
       <li>Simple SEO meta fields</li>
       <li>404 page</li>
-    </ul>
-    <p>Ideas for future versions:</p>
-    <ul>
-      <li>Contact page with forms, icons, and google maps</li>
-      <li>Animations with page scroll</li>
-      <li>Author page and archive</li>
-      <li>Pagination</li>
-      <li>Isotope sorting on Portfolio page</li>
-      <li>Search form in menu</li>
-      <li>Submenus</li>
-      <li>Recaptcha</li>
-      <li>User authentication</li>
-      <li>Mailchimp Email collection</li>
-      <li>Sitemap</li>
-      <li>Google Analytics</li>
-      <li>A/B Testing</li>
-      <li>PWA plugin</li>
+      <li>Offline accessability</li>
     </ul>
   </Layout>
 )
